5859649706395548fec34ee272571ebcbfe407a0
İlham Süleymanov <ilhamsuleymanov40@gmail.com>
2024-04-28 01:48:21 +0400

    Add CencorView with sensor cards to display soil data

--- lib/view/cencor_view.dart
+++ lib/view/cencor_view.dart
@@ -7,6 +7,116 @@ class CencorView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      physics: const BouncingScrollPhysics(),
+      children: const [
+        SizedBox(height: 16),
+        Center(
+          child: Text(
+            'Sensors',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "Harnessing Real-Time Soil Data: Revolutionizing Agriculture with Driven Sensor Technology.",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: 24),
+        SensorCard(),
+        SensorCard(),
+        SensorCard(),
+      ],
+    );
+  }
+}
+
+class SensorCard extends StatelessWidget {
+  const SensorCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24),
+        border: Border.all(
+          color: const Color(0xFF25A047),
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/components/image.jpg'),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            height: 140,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Wheat Field 1",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      "5 Ha - Ağdam",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 4,
+                    vertical: 6,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFEDEDED),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      Icon(Icons.edit, color: Theme.of(context).primaryColor),
+                      const SizedBox(height: 10),
+                      Icon(Icons.settings,
+                          color: Theme.of(context).primaryColor),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
