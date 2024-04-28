4228a1581d2b52d2865abfef2587313230b3a08e
İlham Süleymanov <ilhamsuleymanov40@gmail.com>
2024-04-28 07:42:00 +0400

    Refactor HomeView widget and related components

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:cookers_app/utils/assets_const.dart';
 import 'package:cookers_app/utils/extension.dart';
@@ -9,16 +11,30 @@ import 'package:fl_chart/fl_chart.dart';
 import '../widgets/home_button.dart';
 
 @RoutePage()
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  List<FlSpot> generateRandomData(int count) {
+    int randomNumber(int min, int max) {
+      return min + (max - min) * (100 * Random().nextDouble()).toInt();
+    }
+    return List.generate(count, (index) {
+      return FlSpot(index.toDouble(), randomNumber(1, 10).toDouble());
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: ListView(
           physics: const BouncingScrollPhysics(
-            parent: const AlwaysScrollableScrollPhysics(),
+            parent:  AlwaysScrollableScrollPhysics(),
           ),
           padding: const EdgeInsets.symmetric(horizontal: 22),
           children: [
@@ -75,93 +91,101 @@ class HomeView extends StatelessWidget {
               ),
               itemCount: 4,
               itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: context.primaryColor),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 14,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          SvgPicture.asset('assets/components/plant.svg'),
-                          const SizedBox(width: 10),
-                          Text(
-                            'CO2',
-                            style: context.largeTextStyle.copyWith(
-                              color: context.primaryColor,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      Text(
-                        '412 ppm',
-                        style: context.largerTextStyle?.copyWith(
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      Flexible(
-                        // Set a fixed height for the chart
-                        child: LineChart(
-                          LineChartData(
-                            gridData: const FlGridData(
-                                show: false), // Turn off grid lines
-                            titlesData: const FlTitlesData(
-                                show: false), // Turn off axis titles
-                            borderData:
-                                FlBorderData(show: false), // Turn off border
-                            lineBarsData: [
-                              LineChartBarData(
-                                spots: [
-                                  const FlSpot(0, 1),
-                                  const FlSpot(1, 3),
-                                  const FlSpot(2, 10),
-                                  const FlSpot(3, 7),
-                                  const FlSpot(4, 9),
-                                  const FlSpot(5, 6),
-                                  const FlSpot(6, 3),
-                                  const FlSpot(7, 4),
-                                ],
-                                barWidth: 5,
-                                isStrokeCapRound: true,
-                                dotData: const FlDotData(
-                                  show: false,
-                                ), // Hide the dot data points
-                                belowBarData: BarAreaData(
-                                  show: true,
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      context.primaryColor.withOpacity(0.2),
-                                      context.primaryColor.withOpacity(0.01),
-                                    ],
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                return ValueWidget(
+                  data: generateRandomData(10),
                 );
               },
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ValueWidget extends StatelessWidget {
+  const ValueWidget({
+    super.key,
+    required this.data,
+  });
+
+  final List<FlSpot> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: context.primaryColor),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 14,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset('assets/components/plant.svg'),
+              const SizedBox(width: 10),
+              Text(
+                'CO2',
+                style: context.largeTextStyle.copyWith(
+                  color: context.primaryColor,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            '412 ppm',
+            style: context.largerTextStyle.copyWith(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Flexible(
+            // Set a fixed height for the chart
+            child: LineChart(
+              LineChartData(
+                gridData: const FlGridData(
+                    show: false), // Turn off grid lines
+                titlesData: const FlTitlesData(
+                    show: false), // Turn off axis titles
+                borderData:
+                    FlBorderData(show: false), // Turn off border
+                lineBarsData: [
+                  LineChartBarData(
+                    color: const Color(0xFF5CD69D),
+                    spots: data,
+                    barWidth: 2,
+                    isStrokeCapRound: true,
+                    dotData: const FlDotData(
+                      show: false,
+                    ), // Hide the dot data points
+                    belowBarData: BarAreaData(
+                      show: true,
+                      gradient: LinearGradient(
+                        colors: [
+                          const Color(0xFF5CD69D).withOpacity(0.4),
+                          const Color(0xFF5CD69D).withOpacity(0),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
