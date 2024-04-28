aa96517848692e0b4cf6ae3f678c9a982a068c1e
tync0 <isgenderlituncay2004@gmail.com>
2024-04-28 07:04:23 +0400

    fix error

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-enum NavigationBarItem { home, cencor, map, profile }
+enum NavigationBarItem { home, cencor, profile }
 
 typedef OnNavigationItemTapped = void Function(NavigationBarItem item);
 
